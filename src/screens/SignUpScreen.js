433ce41bf374d6a8720ae45626dbca15c5c53cff
sunyu-pang <pangsunyu@gmail.com>
2021-11-18 20:48:15 +0900

    Styles: Fix typo of SignUpScreen Login ->Log in

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -79,7 +79,7 @@ export default function RegisterScreen({ navigation }) {
       <View style={styles.row}>
         <Text>Already have an account? </Text>
         <TouchableOpacity onPress={() => navigation.replace("LoginScreen")}>
-          <Text style={styles.link}>Login</Text>
+          <Text style={styles.link}>Log in</Text>
         </TouchableOpacity>
       </View>
     </View>
