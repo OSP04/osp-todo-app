003f80aab25215dca37a8b684c871d1b039cfda9
yyeonkim <seyeon.dev@gmail.com>
2021-11-18 22:18:38 +0900

    Fix: Fix CheckBox address

--- src/images.js
+++ src/images.js
@@ -1,4 +1,4 @@
-import CheckedBox from "../assets/baseline_check_box_outline_blank_black_24dp.png";
+import CheckedBox from "../assets/baseline_check_box_black_24dp.png";
 import Add from "../assets/outline_add_black_24dp.png";
 import UncheckedBox from "../assets/baseline_check_box_outline_blank_black_24dp.png";
 import Move from "../assets/outline_import_export_black_24dp.png";
