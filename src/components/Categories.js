b37a8df91261e57f8fce27a04a5ea2fec90e173f
jyjyeon <k-j_y@naver.com>
2021-11-19 11:01:27 +0900

    Add: Create Categories.js

---
+++ src/components/Categories.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { Dimensions } from "react-native";
+
+import styled from "styled-components/native";
+import { images } from "../images";
+import { theme } from "../theme";
+import IconButton from "./IconButton";
+
+const Categories = ({ item }) => {
+
+    const width = Dimensions.get('window').width;
+
+    return (
+        <Wrapper>
+            <StyledView width={width}>
+                <StyledText>{item.text}</StyledText>
+
+                <TaskView width={width}>
+                    <IconButton type={images.uncomplete} />
+                    <TaskText>{item.task1}</TaskText>
+                </TaskView>
+
+                <TaskView width={width}>
+                    <IconButton type={images.uncomplete} />
+                    <TaskText>{item.task2}</TaskText>
+                </TaskView>
+
+                <TaskView width={width}>
+                    <IconButton type={images.uncomplete} />
+                    <TaskText>{item.task3}</TaskText>
+                </TaskView>
+
+                <TaskView width={width}>
+                    <IconButton type={images.uncomplete} />
+                    <TaskText>{item.task4}</TaskText>
+                </TaskView>
+
+                <MoreView width={width}>
+                    <MoreButton title="See more tasks...">See more tasks...</MoreButton>
+                </MoreView>
+            </StyledView>
+        </Wrapper>
+    );
+};
+
+const Wrapper = styled.SafeAreaView`
+flex: 1;
+justify-content: flex-start;
+align-items: center;
+`;
+
+const StyledView = styled.View`
+flex-direction: column;
+align-items: flex-start;
+justify-content: flex-start;
+margin-left: 5px;
+`;
+
+const TaskView = styled.View`
+flex-direction: row;
+align-items: flex-end;
+justify-content: flex-start;
+`;
+
+const MoreView = styled.View`
+align-items: flex-end;
+padding-right: 30px;
+`;
+
+const StyledText = styled.Text`
+font-weight: bold;
+font-size: 22px;
+margin-top: 20px;
+margin-bottom: 6px;
+`;
+
+const TaskText = styled.Text`
+font-size: 18px;
+padding-top: 2px;
+padding-bottom: 2px;
+`;
+
+const MoreButton = styled.Button`
+background-color: ${theme.background};
+color: ${theme.primary};
+`;
+
+export default Categories;
