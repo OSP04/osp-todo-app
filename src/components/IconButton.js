21c95f4aee4fa6d6a48b87d15c02a0039f35a961
jyjyeon <k-j_y@naver.com>
2021-11-20 18:36:45 +0900

    Design: Add icons

---
+++ src/components/IconButton.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Pressable } from "react-native";
+
+import styled from "styled-components/native";
+
+const IconButton = ({ onPressOut, type }) => {
+    return (
+        <Pressable onPressOut={onPressOut}>
+            <Icon source={type} />
+        </Pressable>
+    );
+};
+
+const Icon = styled.Image`
+width: 25px;
+height: 25px;
+`;
+
+export default IconButton;
---
+++ src/images.js
@@ -0,0 +1,5 @@
+import Back from "../assets/baseline_arrow_back_black_24.png";
+
+export const images = {
+    back: Back,
+};
