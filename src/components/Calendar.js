35f1787ee9d27b4b0eafdb987c9ca8885b41c6d2
songing01 <79829662+songing01@users.noreply.github.com>
2021-11-21 03:43:40 +0900

    Add: add all basic parameters of calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -33,13 +33,35 @@ export default class Example extends React.Component {
           }}
           // Hide month navigation arrows. Default = false
           hideArrows={true}
+          // Replace default arrows with custom ones (direction can be 'left' or 'right')
+          renderArrow={(direction) => <Arrow />}
           // Do not show days of other months in month page. Default = false
-          hideExtraDays={true}
-          // If hideArrows=false and hideExtraDays=false do not swich month when tapping on greyed out
+          hideExtraDays={false}
+          // If hideArrows = false and hideExtraDays = false do not switch month when tapping on greyed out
           // day from another month that is visible in calendar page. Default = false
-          disableMonthChange={true}
-          // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+          disableMonthChange={false}
+          // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
           firstDay={7}
+          // Hide day names. Default = false
+          hideDayNames={false}
+          // Show week numbers to the left. Default = false
+          showWeekNumbers={false}
+          // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+          onPressArrowLeft={(subtractMonth) => subtractMonth()}
+          // Handler which gets executed when press arrow icon right. It receive a callback can go next month
+          onPressArrowRight={(addMonth) => addMonth()}
+          // Disable left arrow. Default = false
+          disableArrowLeft={false}
+          // Disable right arrow. Default = false
+          disableArrowRight={false}
+          // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
+          disableAllTouchEventsForDisabledDays={true}
+          // Replace default month and year title with custom one. the function receive a date as parameter
+          renderHeader={(date) => {
+            /*Return JSX*/
+          }}
+          // Enable the option to swipe between months. Default = false
+          enableSwipeMonths={true}
         />
       </View>
     );
