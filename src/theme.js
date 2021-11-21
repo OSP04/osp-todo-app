eb71310f3c1c6ae9e7aa4cc16370022ce95b32c8
songing01 <79829662+songing01@users.noreply.github.com>
2021-11-22 00:53:57 +0900

    Design: Add background theme

--- src/theme.js
+++ src/theme.js
@@ -1,13 +1,14 @@
 import { DefaultTheme } from "react-native-paper";
 
 export const theme = {
-    ...DefaultTheme,
-    colors: {
-        ...DefaultTheme.colors,
-        text: "#FFFFFF",
-        primary: "#24292F",
-        secondary: "#24292F",
-        light: "#BDBDBD",
-        error: "#f13a59",
-    },
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    text: "#FFFFFF",
+    primary: "#24292F",
+    secondary: "#24292F",
+    light: "#BDBDBD",
+    error: "#f13a59",
+  },
+  background: "#FFFFFF",
 };
