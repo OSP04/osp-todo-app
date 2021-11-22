e3d8b54c08a03e78c10c2197f329639b5964fbaf
yyeonkim <seyeon.dev@gmail.com>
2021-11-22 18:25:29 +0900

    Fix: Add move icon deleted when merge

--- src/images.js
+++ src/images.js
@@ -2,10 +2,12 @@ import Back from "../assets/baseline_arrow_back_black_24.png";
 import Add from "../assets/baseline_add_black_24.png";
 import UncheckedBox from "../assets/baseline_check_box_outline_blank_black_24.png";
 import CheckedBox from "../assets/outline_check_box_black_24.png";
+import Move from "../assets/outline_import_export_black_24dp.png";
 
 export const images = {
   back: Back,
   add: Add,
   uncomplete: UncheckedBox,
   complete: CheckedBox,
+  move: Move,
 };
