502f597c40d7cfc207d641c344b9a819ec659919
songing01 <79829662+songing01@users.noreply.github.com>
2021-11-22 16:13:59 +0900

    Feat: Create Calendar screen && importing CalendarBox

---
+++ src/screens/CalendarScreen.js
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import React from "react";
+import CalendarBox from "../components/CalendarBox";
+export default CalendarScreen = () => {
+  return (
+    <View style={{ paddingTop: 50, flex: 1 }}>
+      <CalendarBox />
+    </View>
+  );
+};
