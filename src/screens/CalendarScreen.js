61cfc523aa898a13e587f0921282b9419dcabb19
songing01 <79829662+songing01@users.noreply.github.com>
2021-11-22 17:54:08 +0900

    Feat: Create scrollable calendar screen

--- src/screens/CalendarScreen.js
+++ src/screens/CalendarScreen.js
@@ -1,10 +1,35 @@
 import { View } from "react-native";
 import React from "react";
+import { Calendar, CalendarList, Agenda } from "react-native-calendars";
 import CalendarBox from "../components/CalendarBox";
 export default CalendarScreen = () => {
+  const unformattedCurrent = new Date();
+  const year = unformattedCurrent.getFullYear();
+  const month = unformattedCurrent.getMonth() + 1;
+  const date = unformattedCurrent.getDate();
+  const current = `${year}-${month >= 10 ? month : "0" + month}-${
+    date >= 10 ? date : "0" + date
+  }`;
   return (
-    <View style={{ paddingTop: 50, flex: 1 }}>
-      <CalendarBox />
+    <View style={{ flex: 1 }}>
+      <CalendarList
+        current={current}
+        // Callback which gets executed when visible months change in scroll view. Default = undefined
+        onVisibleMonthsChange={(months) => {
+          console.log("now these months are visible", months);
+        }}
+        // Max amount of months allowed to scroll to the past. Default = 50
+        pastScrollRange={50}
+        // Max amount of months allowed to scroll to the future. Default = 50
+        futureScrollRange={50}
+        // Enable or disable scrolling of calendar list
+        scrollEnabled={true}
+        // Enable or disable vertical scroll indicator. Default = false
+        showScrollIndicator={true}
+        onDayPress={(day) => {
+          console.log("selected day", day);
+        }}
+      />
     </View>
   );
 };
