3dd10c05ebf3430d8c1db403315b0099d24e6871
songing01 <79829662+songing01@users.noreply.github.com>
2021-11-24 02:25:55 +0900

    Design: change search bar's position

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,13 +1,20 @@
 import { SearchBar } from "react-native-elements";
 import React, { useState } from "react";
-import { View } from "react-native";
-
+import { getStatusBarHeight } from "react-native-status-bar-height";
+import { StyleSheet, Text, View } from "react-native";
 export default SearchField = () => {
   return (
     <SearchBar
       placeholder="Type Here..."
       //onChangeText={this.updateSearch}
-      value={search}
+      //value={search}
+      containerStyle={styles.searchBar}
     />
   );
 };
+
+const styles = StyleSheet.create({
+  searchBar: {
+    top: 38 + getStatusBarHeight(),
+  },
+});
