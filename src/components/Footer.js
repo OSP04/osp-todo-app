dd2540335fa13bc24e6949b20a9cf2f3ca976234
yyeonkim <seyeon.dev@gmail.com>
2021-11-24 00:49:54 +0900

    Feat: Add Footer component to Home.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Text } from "react-native";
 
 import styled from "styled-components/native";
 
 import { images } from "../images";
+import { theme } from "../theme";
 import IconButton from "./IconButton";
 
 const Footer = ({ navigation }) => {
@@ -12,14 +14,29 @@ const Footer = ({ navigation }) => {
         type={images.comment}
         onPressOut={() => navigation.navigate("Comments")}
       />
+      <SelectButton>
+        <Text>Select</Text>
+      </SelectButton>
     </StyledView>
   );
 };
 
 const StyledView = styled.View`
-  background-color: tomato;
+  flex: 1;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 5%;
   position: absolute;
   bottom: 0;
+`;
+
+const SelectButton = styled.Pressable`
+  border: 1px solid ${theme.primary};
+  border-radius: 12px;
+  padding: 5px 8px 5px 8px;
+  color: ${theme.text};
 `;
 
 export default Footer;
