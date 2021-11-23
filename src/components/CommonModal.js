255e7e0ff20fdfee3ccea38f690f1098ce8afc89
sunyu-pang <pangsunyu@gmail.com>
2021-11-24 05:24:35 +0900

    Feat: Add CommonModal

---
+++ src/components/CommonModal.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Modal, StyleSheet, View } from "react-native";
+
+const CommonModal = ({
+  showModal,
+  setShowModal,
+  children, // 컴포넌트를 자식으로 넘겨받는다.
+}) => {
+  return (
+    <>
+      {showModal ? (
+        <View style={styles.centeredView}>
+          <Modal
+            animationType="fade"
+            transparent={true}
+            visible={showModal}
+            onRequestClose={() => {
+              setShowModal(!showModal);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>{children}</View>
+            </View>
+          </Modal>
+        </View>
+      ) : null}
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+});
+
+export default CommonModal;
