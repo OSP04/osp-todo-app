0f886f6e8b39d99b4d078ea473cf7a265754c568
yyeonkim <seyeon.dev@gmail.com>
2021-11-24 01:51:54 +0900

    Feat: Add comments' header

--- src/screens/Comments.js
+++ src/screens/Comments.js
@@ -1,10 +1,43 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
+import styled from "styled-components/native";
+
+import IconButton from "../components/IconButton";
 import { db } from "../db";
+import { images } from "../images";
+import { theme } from "../theme";
 
-const Comments = () => {
-  return <Text>Comments</Text>;
+const Comments = ({ navigation }) => {
+  return (
+    <Wrapper>
+      <Header>
+        <IconButton
+          type={images.back}
+          onPressOut={() => navigation.navigate("Home")}
+        />
+        <Title>Comments</Title>
+        <IconButton />
+      </Header>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.View`
+  padding: 5%;
+  background-color: ${theme.background};
+  flex: 1;
+`;
+
+const Header = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Title = styled.Text`
+  font-weight: bold;
+  font-size: 20px;
+`;
 
 export default Comments;
