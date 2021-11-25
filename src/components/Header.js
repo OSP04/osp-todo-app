919f31074b53c59cf93ef427ada631026b97cbe2
sunyu-pang <pangsunyu@gmail.com>
2021-11-25 16:27:45 +0900

    Revert "Feat: Add Header component"
    This reverts commit 9c2221c1c6aa96c5b2dbc6aea23bb2946be12787.

--- src/components/Header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { StyleSheet } from 'react-native'
-import { Text } from 'react-native-paper'
-
-export default function Header(props) {
-  return <Text style= {...props} />
-}
