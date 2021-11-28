568ccf130b9be4566e7fe1c6b374421e6c9868fd
songing01 <79829662+songing01@users.noreply.github.com>
2021-11-29 04:41:33 +0900

    Feat: Get array of starting dates of each task and show with dots

--- src/screens/CalendarScreen.js
+++ src/screens/CalendarScreen.js
@@ -11,7 +11,65 @@ const formatDate = (newDate) => {
   }`;
   return formattedDate;
 };
-export default CalendarScreen = (navigation) => {
+const tasks = [
+  {
+    id: "1",
+    text: "Buy Pizza",
+    date: new Date("2021-11-10T03:00:00.000Z"),
+    due: null,
+    category: "Food",
+    image:
+      "https://images.unsplash.com/photo-1637004253818-d3072efc73fe?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80",
+    complete: false,
+    created: new Date().setDate(10),
+    owner: null,
+  },
+  {
+    id: "2",
+    text: "OpenSW Assignment",
+    date: new Date("2021-11-20T03:00:00.000Z"),
+    due: new Date("2021-11-23T03:00:00.000Z"),
+    category: "School",
+    image: null,
+    complete: true,
+    created: new Date().setDate(20),
+  },
+  {
+    id: "3",
+    text: "Buy noodle",
+    date: new Date("2021-11-15T03:00:00.000Z"),
+    due: new Date("2021-11-17T03:00:00.000Z"),
+    category: "Food",
+    image: null,
+    complete: true,
+    created: new Date().setDate(15),
+    owenr: null,
+  },
+  {
+    id: "4",
+    text: "Submit report",
+    date: new Date("2021-11-16T03:00:00.000Z"),
+    due: null,
+    category: "School",
+    image: null,
+    complete: false,
+    created: new Date().setDate(16),
+    owenr: null,
+  },
+];
+
+const markingDates = tasks.map((task) => formatDate(task.date));
+
+const mark = {};
+markingDates.forEach((day) => {
+  mark[day] = {
+    marked: true,
+    dotColor: "red",
+  };
+});
+
+const CalendarScreen = (navigation) => {
+  console.log(markingDates);
   return (
     <View style={{ paddingtop: 50, flex: 1 }}>
       <BackButton onPressOut={() => navigation.goBack()} />
@@ -34,81 +92,9 @@ export default CalendarScreen = (navigation) => {
           console.log("selected day", day);
         }}
         markingType={"period"}
-        markedDates={{
-          [formatDate(new Date())]: {
-            startingDay: true,
-            color: "green",
-            endingDay: true,
-          },
-          "2021-11-15": { marked: true, dotColor: "#50cebb" },
-          "2021-11-16": { marked: true, dotColor: "#50cebb" },
-          "2021-11-21": {
-            startingDay: true,
-            color: "#50cebb",
-            textColor: "white",
-          },
-          "2021-11-22": { color: "#70d7c7", textColor: "white" },
-          "2021-11-23": {
-            color: "#70d7c7",
-            textColor: "white",
-            marked: true,
-            dotColor: "white",
-          },
-          "2021-11-24": { color: "#70d7c7", textColor: "white" },
-          "2021-11-25": {
-            endingDay: true,
-            color: "#50cebb",
-            textColor: "white",
-          },
-        }}
+        markedDates={mark}
       />
     </View>
   );
 };
-const marking = () => {};
-const [tasks] = [
-  {
-    id: "1",
-    text: "Buy Pizza",
-    date: new Date("2021-11-10T03:00:00.000Z"),
-    due: null,
-    category: "Food",
-    image:
-      "https://images.unsplash.com/photo-1637004253818-d3072efc73fe?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80",
-    complete: false,
-    created: new Date().setDate(10),
-    owner: null,
-  },
-  {
-    id: "2",
-    text: "OpenSW Assignment",
-    date: new Date("2021-11-20T03:00:00.000Z"),
-    due: "2021. 12. 15",
-    category: "School",
-    image: null,
-    complete: true,
-    created: new Date().setDate(20),
-  },
-  {
-    id: "3",
-    text: "Buy noodle",
-    date: new Date("2021-11-15T03:00:00.000Z"),
-    due: "2022.1.5",
-    category: "Food",
-    image: null,
-    complete: true,
-    created: new Date().setDate(15),
-    owenr: null,
-  },
-  {
-    id: "4",
-    text: "Submit report",
-    date: new Date("2021-11-16T03:00:00.000Z"),
-    due: null,
-    category: "School",
-    image: null,
-    complete: false,
-    created: new Date().setDate(16),
-    owenr: null,
-  },
-];
+export default CalendarScreen;
