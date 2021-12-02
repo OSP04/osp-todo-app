e56ee84ce0451b7778e987f9891e24b51ba04514
sunyu-pang <pangsunyu@gmail.com>
2021-12-03 01:40:21 +0900

    Refactor: Add radio buttons

--- src/components/edit/EditCategory.js
+++ src/components/edit/EditCategory.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, Pressable, View, FlatList } from "react-native";
-import { FontAwesome5 } from "@expo/vector-icons";
+import { FontAwesome5, MaterialCommunityIcons } from "@expo/vector-icons";
 import { theme } from "../../theme";
 import { db } from "../../db";
 import CommonModal from "../common/CommonModal";
+import { useEffect } from "react";
+
+const categoryData = db.categories;
 
 const EditCategory = ({}) => {
   const [category, setCategory] = useState("Category");
-  const categoryData = db.categories;
+  const [selectedId, setSelectedId] = useState("");
+  useEffect(() => {}, [selectedId]);
   const [showModal, setShowModal] = useState(false);
   const openModal = () => {
     setShowModal((prev) => !prev);
@@ -16,8 +20,11 @@ const EditCategory = ({}) => {
   const renderCategory = ({ item }) => (
     <Pressable
       onPress={() => {
+        setSelectedId(item.id);
         setCategory(item.title);
-        setShowModal(false);
+        setTimeout(() => {
+          setShowModal(false);
+        }, 50);
       }}
     >
       <View
@@ -29,10 +36,27 @@ const EditCategory = ({}) => {
           borderBottomColor: "#E5E5E5",
         }}
       >
-        <View style={{ width: 60 }}>
-          <Text style={{ fontSize: 17, textAlign: "center" }}>
-            {item.title}{" "}
-          </Text>
+        <View
+          style={{
+            width: "100%",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text style={{ fontWeight: "bold", fontSize: 17 }}>{item.title}</Text>
+          {selectedId === item.id ? (
+            <MaterialCommunityIcons
+              name="radiobox-marked"
+              size={24}
+              color={theme.colors.primary}
+            />
+          ) : (
+            <MaterialCommunityIcons
+              name="radiobox-blank"
+              size={24}
+              color={theme.colors.primary}
+            />
+          )}
         </View>
       </View>
     </Pressable>
