a39d7186c1c3c35d3cb12a1bdbc86020ead91fe2
jyjyeon <k-j_y@naver.com>
2021-12-06 18:53:40 +0900

    Remove: Delete MenuBar.js

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useRef } from "react";
 import ScrollableTabView, {
   ScrollableTabBar,
 } from "react-native-scrollable-tab-view";
@@ -7,18 +7,21 @@ import { theme } from "../../src/theme";
 import AchievementCategory from "./achievement/AchievementCategory";
 import AchievementDay from "./achievement/AchievementDay";
 
-export default class MenuBar extends Component {
-  render() {
-    return (
-      <ScrollableTabView
-        renderTabBar={() => <ScrollableTabBar style={{ marginBottom: 10 }} />}
-        tabBarInactiveTextColor={theme.light}
-        tabBarActiveTextColor={theme.primary}
-        initialPage={0}
-      >
-        <AchievementCategory tabLabel={"Category"} />
-        <AchievementDay tabLabel={"Day"} />
-      </ScrollableTabView>
-    );
-  }
+const MenuBar = () => {
+  const ref = useRef("");
+
+  return (
+    <ScrollableTabView
+      ref={ref}
+      renderTabBar={() => <ScrollableTabBar style={{ marginBottom: 10 }} />}
+      tabBarInactiveTextColor={theme.light}
+      tabBarActiveTextColor={theme.primary}
+      initialPage={0}
+    >
+      <AchievementCategory tabLabel={"Category"} />
+      <AchievementDay tabLabel={"Day"} />
+    </ScrollableTabView>
+  );
 }
+
+export default MenuBar;
