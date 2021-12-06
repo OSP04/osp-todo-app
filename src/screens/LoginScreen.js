ce7d60df232a0a35d6412d9a19e5f75c560b762b
jyjyeon <k-j_y@naver.com>
2021-12-07 07:52:52 +0900

    Remove: Remove screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -56,7 +56,7 @@ export default function LoginScreen({ navigation }) {
           <Text style={styles.link}>Sign up</Text>
         </TouchableOpacity>
       </View>
-      <TouchableOpacity onPress={() => navigation.navigate("Achievement")}>
+      <TouchableOpacity onPress={() => navigation.navigate("EditScreen")}>
         <Text style={styles.skip}>Skip for Now</Text>
       </TouchableOpacity>
     </Background>
