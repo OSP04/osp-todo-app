99bab633aa1ef987d5c5058d7c6e294f4d1e21ab
songing01 <79829662+songing01@users.noreply.github.com>
2021-12-06 14:03:47 +0900

    Feat: Store data

--- App.js
+++ App.js
@@ -2,8 +2,15 @@ import React from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
 import DrawerNavigator from "./DrawerNavigator";
+import { db, storeData } from "./src/db";
 
 function App() {
+  useEffect(() => {
+    storeData("tasks", db.tasks);
+    storeData("categories", db.categories);
+    storeData("users", db.users);
+    storeData("comments", db.comments);
+  }, []);
   return (
     <NavigationContainer>
       <DrawerNavigator />
