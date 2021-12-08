ff2fda85eefa7ba8963ebd315409b9e58557fb8a
sunyu-pang <pangsunyu@gmail.com>
2021-12-09 01:57:59 +0900

    Rename: Separate EditButtons

---
+++ src/components/edit/EditButtons.js
@@ -0,0 +1,120 @@
+import { theme } from "../../theme";
+import React, { useState, useEffect, useContext } from "react";
+import { StyleSheet, View, Alert, Pressable, Text } from "react-native";
+import EditTaskContext from "../../context/EditTask";
+import {
+  addTodo,
+  removeTodo,
+  updateTodo,
+  updateCategories,
+} from "../../editTasksFunc";
+import { storeData, getData } from "../../db";
+
+const EditButtons = ({ selectedTask, goHome }) => {
+  const {
+    editingTitle,
+    editingImage,
+    editingDate,
+    editingDue,
+    editingLocation,
+    editingMemo,
+    editingCategory,
+    editingId,
+    isAddPressed,
+  } = useContext(EditTaskContext);
+
+  const [tasks, setTasks] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(async () => {
+    try {
+      const taskObjs = await getData("tasks");
+      setTasks(taskObjs);
+      const categoryObjs = await getData("categories");
+      setCategories(categoryObjs);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const onDeletePressed = () => {
+    Alert.alert("Delete", "Do you really want to delete this todo?", [
+      {
+        text: "Cancel",
+        onPress: () => null,
+        style: "cancel",
+      },
+      {
+        text: "OK",
+        onPress: () => {
+          removeTodo(tasks, editingId);
+          goHome();
+        },
+      },
+      { cancelable: true },
+    ]);
+  };
+
+  const onConfirmPressed = () => {
+    const task = {
+      ...selectedTask,
+      title: editingTitle,
+      image: editingImage,
+      date: editingDate,
+      due: editingDue,
+      location: editingLocation,
+      memo: editingMemo,
+    };
+    if (isAddPressed) {
+      addTodo(tasks, task);
+    } else {
+      updateTodo(tasks, task, editingId);
+    }
+    updateCategories(
+      categories,
+      editingCategory,
+      editingCategory.tasks,
+      task,
+      editingCategory.id
+    );
+    goHome();
+  };
+  return (
+    <View style={styles.row}>
+      <Pressable style={styles.button} onPress={onDeletePressed}>
+        <Text style={styles.buttonText}>Delete</Text>
+      </Pressable>
+      <Pressable
+        style={{
+          ...styles.button,
+          backgroundColor: theme.colors.primary,
+        }}
+        onPress={onConfirmPressed}
+      >
+        <Text style={{ ...styles.buttonText, color: "white" }}>Confirm</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: 5,
+    padding: 7,
+    borderRadius: 50,
+    width: 80,
+    borderColor: theme.colors.primary,
+    borderWidth: 1,
+  },
+  buttonText: {
+    fontWeight: "bold",
+    color: theme.colors.primary,
+  },
+});
+
+export default EditButtons;
