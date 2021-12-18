630500fd8da1c4b47454678bfe3c0c088d2b57a3
songing01 <79829662+songing01@users.noreply.github.com>
2021-12-19 04:26:42 +0900

    Fix: useFocusEffect on search

--- src/components/search/useSearchTask.js
+++ src/components/search/useSearchTask.js
@@ -1,34 +1,31 @@
 import React, { useState, useEffect, useRef } from "react";
 import SearchedTask from "./SearchedTask";
 import { getData } from "../../db";
-
-const useDidMountEffect = (func, deps) => {
-  const didMount = useRef(false);
-
-  useEffect(() => {
-    if (didMount.current) func();
-    else didMount.current = true;
-  }, deps);
-};
+import { useFocusEffect } from "@react-navigation/native";
 
 function useSearchTask(query) {
   const [searchQuery, setSearchQuery] = useState(query);
   const [filteredTasks, setFilteredTasks] = useState([]);
   const [tasks, setTasks] = useState(null);
+
   const [categories, setCategories] = useState(null);
 
-  useEffect(async () => {
-    try {
-      const taskObjs = await getData("tasks");
-      const categoryObjs = await getData("categories");
-      setTasks(taskObjs);
-      setCategories(categoryObjs);
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
+  useFocusEffect(
+    React.useCallback(async () => {
+      try {
+        const taskObjs = await getData("tasks");
+        const categoryObjs = await getData("categories");
+        setTasks(taskObjs);
+        setCategories(categoryObjs);
+        searchedTask();
+      } catch (error) {
+        console.log(error);
+      }
+      return setSearchQuery(null), setFilteredTasks(null);
+    }, [])
+  );
 
-  useDidMountEffect(() => {
+  useEffect(() => {
     const filterTasks = async () => {
       setFilteredTasks(
         tasks.filter(
@@ -52,20 +49,20 @@ function useSearchTask(query) {
     return category;
   };
 
-  const searchedTask = filteredTasks.map((task, index) => (
-    <SearchedTask
-      task={task}
-      key={task.created}
-      index={index}
-      text={task.text}
-      categoryObj={findCategory(task)}
-      category={task.category}
-      date={task.date.split("T")[0]}
-      due={task.due && task.due.split("T")[0]}
-    />
-  ));
-
-  console.log(searchedTask);
+  const searchedTask =
+    filteredTasks &&
+    filteredTasks.map((task, index) => (
+      <SearchedTask
+        task={task}
+        key={task.created}
+        index={index}
+        text={task.text}
+        categoryObj={findCategory(task)}
+        category={task.category}
+        date={task.date.split("T")[0]}
+        due={task.due && task.due.split("T")[0]}
+      />
+    ));
 
   return { searchQuery, setSearchQuery, searchedTask };
 }
